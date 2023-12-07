7f5dd99b12598ee4f180024c81f01e5f711bfe43
AnthoLaMalice <78861013+AnthoLaMalice@users.noreply.github.com>
2023-12-07 15:06:11 +0100

    Add files via upload

--- CitrixBleed_NSLogs_Parser.py
+++ CitrixBleed_NSLogs_Parser.py
@@ -1,16 +1,18 @@
 import glob
 import re
 from colorama import init, Fore
-
+init(autoreset=True)
 
 def extract_ips(log):
     client_ip_match = re.search(r'Client_ip (\d+\.\d+\.\d+\.\d+)', log)
     source_ip_match = re.search(r'Source (\d+\.\d+\.\d+\.\d+)', log)
+    username_match = re.search(r'Context ([\w.]+)@', log)
 
-    if client_ip_match and source_ip_match:
-        return client_ip_match.group(1), source_ip_match.group(1)
-    else:
-        return None, None
+    client_ip = client_ip_match.group(1) if client_ip_match else None
+    source_ip = source_ip_match.group(1) if source_ip_match else None
+    username = username_match.group(1) if username_match else None
+
+    return client_ip, source_ip, username
 
 path = input("Enter ns.log path. (Usually /var/log) : ")
 
@@ -26,9 +28,8 @@ for files in files_to_open:
 logs = files_content.splitlines()
 
 for log in logs:
-    client_ip, source_ip = extract_ips(log)
+    client_ip, source_ip, username = extract_ips(log)
     if client_ip and source_ip:
         if client_ip != source_ip:
-            print(Fore.RED + f"IP mismatch showing potential exploitation of CitrixBleed (CVE-2023-4966) : Client_ip = {client_ip}, Source = {source_ip}")
-            print(Fore.BLUE + f"Log {log}\n")
-
+            print(Fore.RED + f"IP mismatch showing potential exploitation of CitrixBleed (CVE-2023-4966) : Client_ip = {client_ip}, Source = {source_ip}, Username = {username}")
+            print(Fore.BLUE + f"Log : {log}\n")
